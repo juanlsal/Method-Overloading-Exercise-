e4ca2aca44299d81d9e5ce3c0e1f491378363762
juan lopez salamanca <juanlopezsalamanca1@gmail.com>
2023-03-13 13:54:16 -0500

    completed

--- MethodOverloading/Program.cs
+++ MethodOverloading/Program.cs
@@ -4,7 +4,43 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+
+            Console.WriteLine(Add(2,3));
+            Console.WriteLine(Add(6.5,9.9));
+            Console.WriteLine(Add(100,30, true));
+            Console.WriteLine(Add(-3, 4, true));
+            Console.WriteLine(Add(1, 3, false));
+
+        }
+
+        static int Add(int x, int y)
+        {
+            return x + y;
+        }
+
+        static double Add(double x, double y)
+        {
+            return x + y;
+        }
+
+        static string Add(int x, int y, Boolean k)
+        {
+            if (k == true)
+            {
+                int z = Add(x, y);
+                if (Add(x, y) == 1)
+                {
+                    return ($"{z} dollar");
+                    }
+                else
+                {
+                    return ($"{z} dollars");
+                }
+            }
+            else
+            {
+                return ("false");
+            }
         }
     }
 }
